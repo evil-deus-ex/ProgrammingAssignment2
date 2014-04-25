bf8471070428376f7dac561792a372d5cf16e6fc
Dzmitry Prychyna <evil.java.deus.ex@gmail.com>
2014-04-24 21:54:01 -0300

    initial commit
    both functions implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cached calculation of the matrix inverse. makeCacheMatrix() - builder function to create the data container.
+## cacheSolve - main function for calculating the inverse of the matrix
 
-## Write a short comment describing this function
+## create and return the data container for the given matrix 'x'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL ## create an empty variable for inverse
+    set <- function(y) { ## declare matrix set function
+        x <<- y ## save matrix 
+        inv <<- NULL ## create an empty variable for inverse 
+    }
+    get <- function() { ## declare matrix get function
+        x ## return given matrix    
+    } 
+    setinverse <- function(inverse) { ## declare set inverse cache function
+        inv <<- inverse ## save inverse in the container    
+    }
+    getinverse <- function() { ## declare get inverse cache function
+        inv ## return current inversed matrix or NULL if not set yet
+    }
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) ## create and return data container
 }
 
 
-## Write a short comment describing this function
+## calculate the inverse of the matrix or if it have been already calculated - return the cached value
+## 'x' should be the result of the makeCacheMatrix() function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse() ## get current inverse cache value
+    if (!is.null(inv)) { ## check did we already store the inverse
+        return(inv) ## if we have cache - return the value and exit the function
+    }
+    data <- x$get() ## get the given matrix
+    inv <- solve(data) ## calculate the inverse
+    x$setinverse(inv) ## save the inverse in the data container
+    inv ## Return a matrix that is the inverse of 'x'
 }
